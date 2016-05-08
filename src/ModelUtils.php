59482ec9bffaf20adb108f2a8725d809404fa773
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2016-05-08 22:51:38 +0300

    Refined validateDoc

--- src/ModelUtils.php
+++ src/ModelUtils.php
@@ -40,17 +40,14 @@ class ModelUtils
             
             $my_doc_key_type = self::getType($my_doc[$key]);
             $v_key = $key;
+            if ($my_key !== null) {
+                $v_key = strval($my_key).".".strval($key);
+            }
             // Does doc has a array that does not exist in model definition.
             if (!isset($my_model[$key])) {
-                if ($my_key !== null) {
-                    $v_key = strval($my_key).".".strval($key);
-                }
                 throw new \Exception("Error for key '".$v_key."' that does not exist in the model");
             } // Is the value of the array[key] again another array? .
             elseif ($my_doc_key_type == "array") {
-                if ($my_key !== null) {
-                    $v_key = strval($my_key).".".strval($key);
-                }
                 // Validate this array too.
                 $my_doc[$key] = self::validateDoc($my_model[$key], $my_doc[$key], $v_key);
                 if (self::getType($my_doc[$key]) != "array") {
@@ -59,15 +56,9 @@ class ModelUtils
             } // Is the value of the array[key] have same variable type
               //that stated in the definition of the model array.
             elseif ($my_doc_key_type != $my_model[$key]['_type']) {
-                if ($my_key !== null) {
-                    $v_key = $my_key." . ".$key;
-                }
                 throw new \Exception("Error for key '".$v_key."'".", ".$my_doc_key_type.
                     " given but it must be ".$my_model[$key]['_type']);
             } else {
-                if ($my_key !== null) {
-                    $v_key = $my_key." . ".$key;
-                }
                 $my_doc[$key] = self::validateDocItem($my_doc[$key], $my_model[$key], $v_key);
             }
         }
